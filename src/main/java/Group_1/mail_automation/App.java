cf750e39a2d51276b69e83cdaf965d976a8e3b27
VTamrakar <VTamrakar@Vivek-E5570.actiance.local>
2018-10-09 14:31:14 +0530

    first test

---
+++ src/main/java/Group_1/mail_automation/App.java
@@ -0,0 +1,13 @@
+package Group_1.mail_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
